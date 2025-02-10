ae79e54c3105b1926cd79b2af4ec8a252111874d
Noel Hoppe <noel.hoppe@mni.thm.de>
2025-02-10 13:19:35 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './styles/modern-normalize.css'
+import './styles/style.css'
+import './styles/components/header.css'
+import './styles/components/hero.css'
+import './styles/components/about.css'
+import './styles/components/featured.css'
+import './styles/components/work.css'
+import './styles/components/contact.css'
+import './styles/components/footer.css'
+import './styles/components/mobile-nav.css'
+import './styles/utils.css' // utility classes with highest priority
+
+import mobileNav from './utils/mobile-nav.js'
+import darkMode from "./utils/theme-toggle.js";
+mobileNav();
+darkMode();
---
+++ src/utils/mobile-nav.js
@@ -0,0 +1,22 @@
+const mobileNav = () => {
+    const headerBtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    let isMobileNavOpen = false;
+
+    headerBtn.addEventListener('click', () => {
+        isMobileNavOpen = !isMobileNavOpen;
+        if (isMobileNavOpen) {
+            mobileNav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+        } else {
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        }
+    });
+}
+export default mobileNav;
+
+
+
+
+
